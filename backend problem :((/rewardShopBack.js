7f1b0732909f2a35eed52033ed9debfaa9303902
applx <154079719+applxk@users.noreply.github.com>
2024-06-10 18:11:19 +0800

    Create rewardShopBack.js

---
+++ backend problem :((/rewardShopBack.js
@@ -0,0 +1,13 @@
+/* client/src/components/rewardShopBack.js */
+
+import React from 'react';
+
+const RewardShopBack = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default RewardShopBack;
