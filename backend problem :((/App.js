cfa2db2323f9837613f8599eae370f5746991e2a
applx <154079719+applxk@users.noreply.github.com>
2024-06-10 18:09:31 +0800

    Create App.js

---
+++ backend problem :((/App.js
@@ -0,0 +1,14 @@
+/* client/src/components/App.js */
+
+import React from 'react';
+import RewardShopBack from './components/rewardShopBack'; // Import RewardShopBack component
+
+function App() {
+  return (
+    <div>
+      <RewardShopBack /> {/* Render the RewardShopBack component */}
+    </div>
+  );
+}
+
+export default App;
